edf2cb9ae98f1970b0d9d68cc4a2bde47491f575
johnkiersznowski <johnkiersznowski@gmail.com>
2023-05-23 02:03:08 -0400

    commit

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "YOUR KEY HERE"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ed2dd63e5c7049af847c618e0b9f662b"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6c4f384a4769352fefe851f08b3f2ed1"
 
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
